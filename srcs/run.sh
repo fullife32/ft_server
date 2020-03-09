5e0854906524664cbfcb2601ce7774ef765e6a4d
Eithan ASSOULINE <eassouli@e2r6p10.42.fr>
2020-03-09 18:57:58 +0100

    ok:

--- srcs/run.sh
+++ srcs/run.sh
@@ -5,8 +5,8 @@ service mysql start
 service php7.3-fpm start
 
 wget https://files.phpmyadmin.net/phpMyAdmin/4.9.4/phpMyAdmin-4.9.4-all-languages.tar.gz \
-mv -f /phpMyAdmin-4.9.4-all-languages/* /var/www/html/phpmyadmin/ \
+mv /phpMyAdmin-4.9.4-all-languages/* /var/www/html/phpmyadmin/ \
 rm /phpMyAdmin-4.9.4-all-languages.tar.gz \
 rm -rf /phpMyAdmin-4.9.4-all-languages
 
-tail -f /var/log/nginx/access.log /var/log/nginx/error.log
+tail /var/log/nginx/access.log /var/log/nginx/error.log
