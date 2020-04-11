30e34b1b5143eab5a93dbbb4450ecf0ff7951273
fullife32 <eithan.assouline6@gmail.com>
2020-04-11 16:40:16 +0200

    move localhost to html

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,40 +1,34 @@
+# Create localhost server
+mv /root/localhost /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
+
+# PHP info index
+mv /root/info.php /var/www/html/
+
+# phpMyAdmin installation and configuration
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.4/phpMyAdmin-4.9.4-all-languages.tar.gz
+mkdir /var/www/html/phpmyadmin
+tar xzf /phpMyAdmin-4.9.4-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpmyadmin/
+rm -rf /phpMyAdmin-4.9.4-all-languages.tar.gz
+mv /root/config.default.php /var/www/html/phpmyadmin/libraries/
+chmod -R 755 /var/www/html/phpmyadmin
+
+# Wordpress installation and configuration
+wget https://wordpress.org/latest.tar.gz
+mkdir /var/www/html/wordpress
+tar xzf /latest.tar.gz --strip-components=1 -C /var/www/html/wordpress/
+rm -rf /latest.tar.gz
+mv /root/wp-config.php /var/www/html/wordpress/
+chmod -R 755 /var/www/html/wordpress
+
+# Configure Wordpress and phpMyAdmin database
+mariadb < /root/db_conf.sql
+mysql < /var/www/html/phpmyadmin/sql/create_tables.sql
+
 # Starting services
 service nginx start
 service mysql start
 service php7.3-fpm start
-
-# Create localhost server
-mkdir /var/www/localhost
-chown -R $USER:$USER /var/www/localhost
-mv /root/localhost /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
-
-# Nginx index
-mv /root/index.nginx-debian.html /var/www/localhost/
-
-# PHP info index
-mv /root/info.php /var/www/localhost/
-
-# phpMyAdmin installation and configuration
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.4/phpMyAdmin-4.9.4-all-languages.tar.gz
-mkdir /var/www/localhost/phpmyadmin
-tar xzf /phpMyAdmin-4.9.4-all-languages.tar.gz --strip-components=1 -C /var/www/localhost/phpmyadmin/
-rm -rf /phpMyAdmin-4.9.4-all-languages.tar.gz
-mv /root/config.default.php /var/www/localhost/phpmyadmin/libraries/
-
-# Wordpress installation and configuration
-wget https://wordpress.org/latest.tar.gz
-mkdir /var/www/localhost/wordpress
-tar xzf /latest.tar.gz --strip-components=1 -C /var/www/localhost/wordpress/
-rm -rf /latest.tar.gz
-mv /root/wp-config.php /var/www/localhost/wordpress/
-
-# Configure Wordpress and phpMyAdmin database
-mariadb < /root/db_conf.sql
-mysql < /var/www/localhost/phpmyadmin/sql/create_tables.sql
-
-# Reload services
-service nginx reload
 
 # Loop server
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log
